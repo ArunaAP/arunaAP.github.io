d16c11be558f5a2cf116bd4462e37dac6001c382
ArunaAP <it20657796@my.sliit.lk>
2023-02-17 00:39:55 +0530

    Update Website

--- script.js
+++ script.js
@@ -1,43 +1,40 @@
- $(document).ready(function(){
-     $(window).scroll(function(){
-         if(this.scrollY > 20){
-             $(".navbar").addClass("sticky");
-         } else{
-             $(".navbar").removeClass("sticky");
-         }
-         if(this.scrollY > 500){
-            $(".scroll-up-btn").addClass("show");
-         }else{
-            $(".scroll-up-btn").removeClass("show");
-         }
-     });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
 
-    //  toggle menu script
-     $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-     });
+  //  toggle menu script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+});
 
- });
-
- //slide up script
- $('.scroll-up-btn').click(function(){
-    $('html').animate({scrollTop: 0});
- });
-
+//slide up script
+$(".scroll-up-btn").click(function () {
+  $("html").animate({ scrollTop: 0 });
+});
 
 //  typing animation script
 var typed = new Typed(".typing", {
-    strings: ["SE Undergraduate", "Developer", "Photographer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-})
-
+  strings: ["SE Undergraduate", "Full Stack Dev", "Photographer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
 
 var typed = new Typed(".typing-2", {
-    strings: ["SE Undergraduate", "Developer", "Photographer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-})
+  strings: ["SE Undergraduate", "Full Stack Dev", "Photographer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
